6833ae28cdd36b4ced3e032a5dda8711cf339ded
sunfu-chou <sunfu.chou@gmail.com>
2022-07-22 20:41:35 +0800

    Feat: Add blender

---
+++ get_blender.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget -c https://ftp.halifax.rwth-aachen.de/blender/release/Blender3.2/blender-3.2.1-linux-x64.tar.xz
+tar xf ./blender-3.2.1-linux-x64.tar.xz
+rm blender-3.2.1-linux-x64.tar.xz
